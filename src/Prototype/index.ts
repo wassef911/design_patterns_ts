ef94239b9a90038da697247c98e61996a5032dad
Wassef911 <1.wassef911@gmail.com>
2022-01-30 21:41:46 +0100

    prototype/cloning

--- src/Prototype/index.ts
+++ src/Prototype/index.ts
@@ -3,9 +3,6 @@ class Prototype {
     public component: object;
     public circularReference: ComponentWithBackReference;
 
-    constructor() {
-    }
-
     public clone(): this {
         const clone = Object.create(this);
 
@@ -30,7 +27,7 @@ class ComponentWithBackReference {
 /**
  * The client code.
  */
-function clientCode() {
+(() => {
     const p1 = new Prototype();
     p1.primitive = 245;
     p1.component = new Date();
@@ -39,28 +36,18 @@ function clientCode() {
     const p2 = p1.clone();
     // will result in error because of circular reference
     //const p2 = JSON.parse(JSON.stringify(p1));
-    if (p1.primitive === p2.primitive) {
+    if (p1.primitive === p2.primitive)
         console.log('Primitive field values have been carried over to a clone. Yay!');
-    } else {
-        console.log('Primitive field values have not been copied. Booo!');
-    }
-    if (p1.component === p2.component) {
-        console.log('Simple component has not been cloned. Booo!');
-    } else {
+
+    if (p1.component !== p2.component)
         console.log('Simple component has been cloned. Yay!');
-    }
 
-    if (p1.circularReference === p2.circularReference) {
-        console.log('Component with back reference has not been cloned. Booo!');
-    } else {
+
+    if (p1.circularReference !== p2.circularReference)
         console.log('Component with back reference has been cloned. Yay!');
-    }
 
-    if (p1.circularReference.prototype === p2.circularReference.prototype) {
-        console.log('Component with back reference is linked to original object. Booo!');
-    } else {
+
+    if (p1.circularReference.prototype !== p2.circularReference.prototype)
         console.log('Component with back reference is linked to the clone. Yay!');
-    }
-}
 
-clientCode();
+})()
